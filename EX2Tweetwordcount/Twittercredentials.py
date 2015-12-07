a61759e061e3e1d265b80bb980e4cc2d720364ea
root <root@ip-172-31-60-90.ec2.internal>
2015-12-07 21:39:30 +0000

    Updated EX2 to reflect topology (but did not modify spouts/bolts), and added credentials to twittercredentials.py

--- EX2Tweetwordcount/Twittercredentials.py
+++ EX2Tweetwordcount/Twittercredentials.py
@@ -1,17 +1,17 @@
 import tweepy
 
 consumer_key = "YourConsumerkey";
-#eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
+#eg: consumer_key = "LFWvQkfmAC0WQoGAxC2jGOea3";
 
 
 consumer_secret = "YourConsumerSecret";
-#eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
+#eg: consumer_secret = "ZLzQBXdpBY1gERO8du5R2jNs8JQlXpN2VtFJ0nm42sdKBKpe48";
 
 access_token = "YourAcessToken";
-#eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
+#eg: access_token = "4489773074-zIsQyDjSSVCwAadtqEEvhKdsCe7t2zHABt65lYc";
 
 access_token_secret = "YourTokenSecret";
-#eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
+#eg: access_token_secret = "Tcto4ynVOSFiqffiadktt4CkjVR1f56Dw9EX15VVH6bnd";
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
