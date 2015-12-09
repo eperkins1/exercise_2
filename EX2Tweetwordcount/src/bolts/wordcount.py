1e66b518c8098e0f2a654bd0c9bab5781d0febd8
eperkins1 <eperkins27@gmail.com>
2015-12-08 22:48:23 -0800

    Everything worked, now Im gonna get rid of log statements, and see how it works to query the postgres table as its running

--- EX2Tweetwordcount/src/bolts/wordcount.py
+++ EX2Tweetwordcount/src/bolts/wordcount.py
@@ -43,4 +43,4 @@ class WordCounter(Bolt):
         self.conn.commit()
 
         # Log the count - just to see the topology running
-        self.log('%s: %d' % (word, self.counts[word]))
+        #self.log('%s: %d' % (word, self.counts[word]))
