f8eb0eb565e012aefd4c327769bef6300e2868e1
eperkins1 <eperkins27@gmail.com>
2015-12-10 12:28:26 -0800

    Fix attempt 3

--- EX2Tweetwordcount/t20plot.py
+++ EX2Tweetwordcount/t20plot.py
@@ -18,7 +18,11 @@ for rec in records:
 	words.append(rec[0])
 	counts.append(int(rec[1]))
 
-wordnums = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39]
+wordnums = []
+curlen = 0
+for word in words:
+	curlen += len(word)
+	wordnums.append(curlen)
 plt.bar(wordnums, counts, align='center')
 plt.xticks(wordnums, words)
 
