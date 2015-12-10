5fbb6b08b4e63c412cd1b8ef88f817c4f40c6520
eperkins1 <eperkins27@gmail.com>
2015-12-09 23:08:54 -0800

    First attempt at histogram file

---
+++ EX2Tweetwordcount/histogram.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python                                                                                                                                                                                       
+from __future__ import absolute_import, print_function, unicode_literals
+import sys
+import psycopg2
+
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+cur = conn.cursor()
+
+if len(sys.argv) != 2:
+	#Case where we print out all the words in the stream and their total count of occurrences, sorted alphabetically in an ascending order, one word per line                                           
+    print ("Please enter two numbers separated by only a comma as single argument, where the first is less than or equal to the second")
+else:
+    ks = sys.argv.split(',')
+    if len(ks) != 2 or !(isinstance(ks[0], (int)) and isinstance(ks[1], (int))) or ks[0] > ks[1]:
+        print("Please enter two numbers separated by only a comma as a single argument, where the first is less than or equal to the second")
+    else:
+        cur.execute("SELECT word, count from Tweetwordcount WHERE count >= %s AND count <= %s" % (ks[0], ks[1]))
+        records = cur.fetchall()
+        for rec in records:
+            print (rec[0] + ": " + str(rec[1]) + "\n")
+
+    # w = str(sys.argv[1])
+    # #Case where we print number of occurrences of sys.argv[1]
+    # cur.execute("SELECT word, count from Tweetwordcount WHERE word = \'%s\'" % (w))
+    # records = cur.fetchall()
+    # for rec in records:
+    #    print ("Total number of occurrences of \"" + rec[0] + "\": " + str(rec[1]) + "\n")
+conn.commit()
+conn.close()
