e419f04bee79679ce780069547b73c5ed6875b42
eperkins1 <eperkins27@gmail.com>
2015-12-09 21:05:18 -0800

    Major updates to finalresults.py, tryign to make it all work now, attempt 1

--- EX2Tweetwordcount/finalresults.py
+++ EX2Tweetwordcount/finalresults.py
@@ -1,11 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python                                                                                                                                                                                       
 from __future__ import absolute_import, print_function, unicode_literals
 import sys
 import psycopg2
 
+conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", host="localhost", port="5432")
+cur = conn.cursor()
 
 if sys.argv[1] is None:
-	#Case
-	print ("you forgot to enter an argument")
+	#Case where we print out all the words in the stream and their total count of occurrences, sorted alphabetically in an ascending order, one word per line                                           
+        cur.execute("SELECT word, count from Tweetwordcount ORDER BY word ASC")
+		records = cur.fetchall()
+
+		for rec in records:
+		   print ("word = ", rec[0])
+		   print ("count = ", rec[1], "\n")
 else:
-	print("The first arg is " + str(sys.argv[1]))
+    #Case where we print number of occurrences of sys.argv[1]
+    cur.execute("SELECT word, count from Tweetwordcount WHERE word == \'%s\'", sys.argv[1])
+    records = cur.fetchall()
+    for rec in records:
+    	print ("Total number of occurrences of \"%s\": %s", (rec[0], rec[1]), "\n")
+conn.commit()
+conn.close()
